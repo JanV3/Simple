c54c8968a133ddb9d2766a57354108ccd20c5a2f
Jan Valiska <jan.valiska@gmail.com>
2019-03-09 15:24:14 +0100

    Rewrite zipper_test using catch2 framework

--- tests/zipper_test.cpp
+++ tests/zipper_test.cpp
@@ -1,20 +1,28 @@
-#include "Simple/Zipper.hpp"
-
 #include <iostream>
 #include <list>
-#include <thread>
+#include <unordered_map>
 #include <vector>
 
-using namespace std;
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-int main()
+#include "Simple/Zipper.hpp"
+
+TEST_CASE("Create map from two containers")
 {
     std::vector<int> ints{10, 20, 30, 40};
     std::list<char> chars{'a', 'b', 'c', 'd'};
 
+    std::unordered_map<int, char> mapped;
     for (auto [i, c] : Simple::Zipper(ints, chars)) {
-        std::cout << "int: " << i << " char: " << c << std::endl;
+        mapped[i] = c;
     }
 
-    return 0;
+    REQUIRE(ints.size() == 4);
+    REQUIRE(chars.size() == 4);
+    REQUIRE(mapped.size() == 4);
+    REQUIRE(mapped[10] == 'a');
+    REQUIRE(mapped[20] == 'b');
+    REQUIRE(mapped[30] == 'c');
+    REQUIRE(mapped[40] == 'd');
 }
